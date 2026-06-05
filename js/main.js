989de6a52e325a4d73a4b5040c76c805f268ed55
xoxo <sung021125@gmail.com>
2026-06-05 13:49:07 +0900

    페이저 수정

--- js/main.js
+++ js/main.js
@@ -14,46 +14,78 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const slideWrapper = document.querySelector(".slidewrapper"),
-  slideContainer = slideWrapper.querySelector(".slidecontainer"),
-  slides = slideContainer.querySelectorAll("li"),
-  slideCount = slides.length,
-  prevBtn = slideWrapper.querySelector(".prev"),
-  nextBtn = slideWrapper.querySelector(".next");
-let currentIdx = 0;
+const slideWrapper = document.querySelector(".slidewrapper");
 
-/* slideContainer 너비 지정 */
-slideContainer.style.width = `${slideCount * 100}%`;
+if (slideWrapper) {
+  const slideContainer = slideWrapper.querySelector(".slidecontainer"),
+    slides = slideContainer.querySelectorAll("li"),
+    slideCount = slides.length,
+    prevBtn = slideWrapper.querySelector(".prev"),
+    nextBtn = slideWrapper.querySelector(".next");
+  let currentIdx = 0;
 
-/* 
-슬라이드 이동 함수 
-moveSlide 함수 생성, 
-이 함수는 숫자 num이 들어오면 
-num 번호에 해당하는 슬라이드 보이도록 이동
-transform:translateX(33.3333%)
-*/
-function moveSlide(num) {
-  slideContainer.style.transform = `translateX(${-(num / slideCount) * 100}%)`;
-  currentIdx = num;
+  /* slideContainer 너비 지정 */
+  slideContainer.style.width = `${slideCount * 100}%`;
+
+  /* pager 생성 및 클릭 이벤트 바인딩 */
+  const pagerContainer = document.createElement("div");
+  pagerContainer.classList.add("pager");
+
+  const pagerBullets = [];
+
+  for (let i = 0; i < slideCount; i++) {
+    const pagerBullet = document.createElement("span");
+    if (i === 0) {
+      pagerBullet.classList.add("active");
+    }
+    pagerBullet.addEventListener("click", () => {
+      moveSlide(i);
+    });
+    pagerContainer.appendChild(pagerBullet);
+    pagerBullets.push(pagerBullet);
+  }
+
+  slideWrapper.appendChild(pagerContainer);
+
+  /* 
+  슬라이드 이동 함수 
+  moveSlide 함수 생성, 
+  이 함수는 숫자 num이 들어오면 
+  num 번호에 해당하는 슬라이드 보이도록 이동
+  transform:translateX(33.3333%)
+  */
+  function moveSlide(num) {
+    slideContainer.style.transform = `translateX(${-(num / slideCount) * 100}%)`;
+    currentIdx = num;
+
+    // pager active 클래스 업데이트
+    pagerBullets.forEach((bullet, idx) => {
+      if (idx === num) {
+        bullet.classList.add("active");
+      } else {
+        bullet.classList.remove("active");
+      }
+    });
+  }
+
+  //버튼으로 이동하기
+  /*
+  다음 버튼을 클릭하면 할일
+    변수명 nextIdx 다음 슬라이드 번호 생성
+    (마지막이면 첫번째 슬라이드로 이동)
+    moveSlide(nextIdx) 실행
+
+  이전버튼을 클릭하면 할일
+    변수명 nextIdx 다음 슬라이드 번호 생성
+    (첫번째 슬라이드면 마지막 슬라이드로 이동)
+    moveSlide(nextIdx) 실행  
+  */
+  nextBtn.addEventListener("click", () => {
+    let nextIdx = (currentIdx + 1) % slideCount;
+    moveSlide(nextIdx);
+  });
+  prevBtn.addEventListener("click", () => {
+    let nextIdx = (currentIdx - 1 + slideCount) % slideCount;
+    moveSlide(nextIdx);
+  });
 }
-
-//버튼으로 이동하기
-/*
-다음 버튼을 클릭하면 할일
-  변수명 nextIdx 다음 슬라이드 번호 생성
-  (마지막이면 첫번째 슬라이드로 이동)
-  moveSlide(nextIdx) 실행
-
-이전버튼을 클릭하면 할일
-  변수명 nextIdx 다음 슬라이드 번호 생성
-  (첫번째 슬라이드면 마지막 슬라이드로 이동)
-  moveSlide(nextIdx) 실행  
-*/
-nextBtn.addEventListener("click", () => {
-  let nextIdx = (currentIdx + 1) % slideCount;
-  moveSlide(nextIdx);
-});
-prevBtn.addEventListener("click", () => {
-  let nextIdx = (currentIdx - 1 + slideCount) % slideCount;
-  moveSlide(nextIdx);
-});
